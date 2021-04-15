4b29b9c18fb10e8b191ffceacfa32905147edea0
Anjali Chaturvedi <anjaliichaturvedi@gmail.com>
2021-04-15 22:30:37 +0530

    modified faqs

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Pitchteen 2.0
 a tech startup competition for womxn.
+
+Register: www.bit.ly/PT2_register
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-$(function () {
-    $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > 10 ) {
-            $('.navbar').addClass('active');
-        } else {
-            $('.navbar').removeClass('active');
-        }
-    });
+$(function() {
+  $(window).on('scroll', function() {
+    if ($(window).scrollTop() > 10) {
+      $('.navbar').addClass('active');
+    } else {
+      $('.navbar').removeClass('active');
+    }
+  });
 });
 
 var acc = document.getElementsByClassName("accordion");
